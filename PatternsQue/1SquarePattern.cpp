6ccfc126047938e836c4ff687a6d9091fafa01ad
shreyas-urankar <urankarshreyas@gmail.com>
2024-09-25 23:25:45 +0530

    First commit

--- PatternsQue/1SquarePattern.cpp
+++ PatternsQue/1SquarePattern.cpp
@@ -4,6 +4,30 @@
 #include <iostream>
 using namespace std;
 
+
+//For number pattern
+//........................................
+
+
+// int main()
+// {
+//     int i,j,n;
+//     cout<<"Enter the n:";
+//     cin>>n;
+//     for(i=1; i<n; i++)
+//     {
+//         for(j=1; j<=n; j++)
+//         {
+//             cout<<j;
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+
+// for star pattern
+// ................................
 int main()
 {
     int i,j,n;
@@ -13,7 +37,7 @@ int main()
     {
         for(j=1; j<=n; j++)
         {
-            cout<<j;
+            cout<<"*"<<" ";
         }
         cout<<endl;
     }
