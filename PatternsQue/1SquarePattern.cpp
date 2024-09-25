320a20ce2b0c3a405b6ee15dd45b2d161b526a9d
shreyas-urankar <urankarshreyas@gmail.com>
2024-09-26 00:02:49 +0530

    First commit

--- PatternsQue/1SquarePattern.cpp
+++ PatternsQue/1SquarePattern.cpp
@@ -46,20 +46,70 @@ using namespace std;
 
 //for characters
 //...........................................
-int main()
+// int main()
+// {
+//     int i,j,n;
+//     cout<<"Enter the n:";
+//     cin>>n;
+//     for(i=0; i<=n; i++)
+//     {
+//         char ch='A';
+//         for(j=0; j<=n; j++)
+//         {
+//             cout<<ch<<" ";
+//             ch=ch+1;
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+//for numbers like 123
+             //    456
+             //    789
+//.........................................
+//  int main()
+// {
+//     int i,j,n;
+//     cout<<"Enter the n:";
+//     cin>>n;
+//     int num=1;
+//     for(i=1; i<=n; i++)
+//     {
+//         for(j=1; j<=n; j++)
+//         {
+//             cout<<num<<" ";
+//             num++;
+//         }
+//         cout<<endl;
+//     }
+//     cout<<"After pattern:"<<num<<endl;
+//     return 0;
+// }
+
+//for alphabets like ABC
+              //     DEF
+             //      GHI
+//.........................................
+
+ int main()
 {
     int i,j,n;
     cout<<"Enter the n:";
     cin>>n;
-    for(i=0; i<=n; i++)
+    char ch='A';
+    for(i=1; i<=n; i++)
     {
-        char ch='A';
-        for(j=0; j<=n; j++)
+        for(j=1; j<=n; j++)
         {
             cout<<ch<<" ";
-            ch=ch+1;
+            ch++;
+            if (ch> 'Z'){
+                ch='A';
+            }
         }
         cout<<endl;
     }
+    cout<<"After pattern:"<<ch<<endl;
     return 0;
 }
